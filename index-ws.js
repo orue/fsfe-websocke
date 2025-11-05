219b34d3c0dd6329f017ecd6085bee673dd7ec39
Carlos Orue <carlos@orue.dev>
2025-11-04 19:54:20 -0600

    Batman

---
+++ index-ws.js
@@ -0,0 +1,13 @@
+const { log } = require('console');
+const express = require('express');
+const server = require('http').createServer();
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+server.on('request', app);
+server.listen(3000, function () {
+  console.log('server started on port 3000');
+});
